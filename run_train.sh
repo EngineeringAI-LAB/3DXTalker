c0985ee58d6ee1b35ca7088486807d247c8c3fa0
wzy <zywang9691@gmail.com>
2026-02-11 11:42:08 +0800

    data process

--- run_train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "Running on node: $(hostname)"
-echo "Starting at: $(date)"
-export CUDA_VISIBLE_DEVICES=0,1
-echo "CUDA_VISIBLE_DEVICES = ${CUDA_VISIBLE_DEVICES:-Not Set}"
-
-accelerate launch --config_file ./accelerate_configs/accelerate_gpus.yaml --mixed_precision no --multi_gpu train.py > train_TerminalLog.log 2>&1 & 
